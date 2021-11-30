83ccb9128b7d71233f4deaf0007322a52d58053a
PabloGarcia123 <93787897+PabloGarcia123@users.noreply.github.com>
2021-11-30 11:31:25 +0100

    Add files via upload

---
+++ Iter1/src/main/java/Dominio/ControlMedicinas.java
@@ -0,0 +1,15 @@
+package Dominio;
+
+public class ControlMedicinas {
+
+	/**
+	 * 
+	 * @param Medicina
+	 * @param id_Persona
+	 */
+	public String cambiarMedicina(String Medicina, String id_Persona) {
+		// TODO - implement ControlMedicinas.cambiarMedicina
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Iter1/src/main/java/Presentacion/IU_ActualizarBD.java
@@ -0,0 +1,11 @@
+package Presentacion;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class IU_ActualizarBD {
+
+	private JFrame Frame;
+	private JTextField txtPulseParaCambiar;
+
+}
