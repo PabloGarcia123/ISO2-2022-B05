5b2b136b2caca6f976ec5f9be56460967ef9bb35
EDMolina <91478468+EDMolina@users.noreply.github.com>
2021-12-17 11:51:36 +0100

    Add files via upload

---
+++ Iter7/src/main/java/Dominio/ControlEstadisticas.java
@@ -0,0 +1,40 @@
+package Dominio;
+
+public class ControlEstadisticas {
+
+	public static int[] numEnfNumPersEstados() {
+		// TODO - implement ControlEstadisticas.numEnfNumPersEstados
+		throw new UnsupportedOperationException();
+	}
+
+	public static int numEnf() {
+		// TODO - implement ControlEstadisticas.numEnf
+		throw new UnsupportedOperationException();
+	}
+
+	public static int[] numCampInf() {
+		// TODO - implement ControlEstadisticas.numCampInf
+		throw new UnsupportedOperationException();
+	}
+
+	public static double[] evolucionEnf() {
+		// TODO - implement ControlEstadisticas.evolucionEnf
+		throw new UnsupportedOperationException();
+	}
+
+	public static double[] vacunacion() {
+		// TODO - implement ControlEstadisticas.vacunacion
+		throw new UnsupportedOperationException();
+	}
+
+	public static double[] relacionVacInf() {
+		// TODO - implement ControlEstadisticas.relacionVacInf
+		throw new UnsupportedOperationException();
+	}
+
+	public static double[] previsionOlas() {
+		// TODO - implement ControlEstadisticas.previsionOlas
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Iter7/src/main/java/Presentacion/IU_Estadisticas.java
@@ -0,0 +1,31 @@
+package Presentacion;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class IU_Estadisticas {
+
+	private JFrame frame;
+	private JTextField txtMostrarEstadistica;
+
+	public void main() {
+		// TODO - implement IU_Estadisticas.main
+		throw new UnsupportedOperationException();
+	}
+
+	public IU_Estadisticas() {
+		// TODO - implement IU_Estadisticas.IU_Estadisticas
+		throw new UnsupportedOperationException();
+	}
+
+	private void initialize() {
+		// TODO - implement IU_Estadisticas.initialize
+		throw new UnsupportedOperationException();
+	}
+
+	private void mostrarMensajeExito() {
+		// TODO - implement IU_Estadisticas.mostrarMensajeExito
+		throw new UnsupportedOperationException();
+	}
+
+}
