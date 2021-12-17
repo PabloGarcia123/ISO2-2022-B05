f48477e1bb9df73f700ea0244be6944e396b2783
NoelFlajelo12 <91826014+NoelFlajelo12@users.noreply.github.com>
2021-12-17 12:15:36 +0100

    Commit iteración 6

---
+++ Iter6/src/main/java/Presentacion/IU_CampañasInformativas.java
@@ -0,0 +1,31 @@
+package Presentacion;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class IU_CampañasInformativas {
+
+	private JFrame frame;
+	private JTextField txtMostrarCampañaInfo;
+
+	public void main() {
+		// TODO - implement IU_Campa�asInformativas.main
+		throw new UnsupportedOperationException();
+	}
+
+	public IU_CampañasInformativas() {
+		// TODO - implement IU_Campa�asInformativas.IU_Campa�asInformativas
+		throw new UnsupportedOperationException();
+	}
+
+	private void initialize() {
+		// TODO - implement IU_Campa�asInformativas.initialize
+		throw new UnsupportedOperationException();
+	}
+
+	private void mostrarMensaje() {
+		// TODO - implement IU_Campa�asInformativas.mostrarMensaje
+		throw new UnsupportedOperationException();
+	}
+
+}
